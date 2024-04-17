3140b3707a78f7c9d32ee1b1372b4c8bcaaa0a1c
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 09:36:29 +0530

    configured multer for FormData

--- authController.js
+++ authController.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./User');
+const multer = require('multer');
+const upload = multer();
 
-router.post('/signup', async (req, res) => {
+router.post('/signup', upload.none(), async (req, res) => {
   const { username, password } = req.body;
-  
+
   if (!username || !password) {
     return res.status(400).send('Username and password are required');
   }
@@ -17,13 +19,13 @@ router.post('/signup', async (req, res) => {
   res.sendStatus(201);
 });
 
-router.post('/login', async (req, res) => {
-    const user = await User.findOne({ username: req.body.username });
-    if (!user || !await bcrypt.compare(req.body.password, user.password)) {
-        return res.sendStatus(401);
-    }
-    const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
-    res.send({ token });
+router.post('/login', upload.none(), async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+  if (!user || !await bcrypt.compare(req.body.password, user.password)) {
+    return res.sendStatus(401);
+  }
+  const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
+  res.send({ token });
 });
 
 module.exports = router;
