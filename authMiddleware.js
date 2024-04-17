abefb2c4a14e5ddf9d929b6e3c45656cfc112866
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 09:47:14 +0530

    bugfix

--- authMiddleware.js
+++ authMiddleware.js
@@ -2,10 +2,12 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (!token) return res.sendStatus(401);
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.sendStatus(401);
+  }
 
-  jwt.verify(token, 'secret_key', (err, user) => { 
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
     next();
