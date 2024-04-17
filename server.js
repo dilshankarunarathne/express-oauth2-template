a71ca9653191d3d9119b104cb00d31e1e9fe9420
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 09:41:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authController);
-app.use('/secured', );
+app.use('/secured', securedController);
 
 app.listen(3000, () => console.log('Server started on port 3000'));
