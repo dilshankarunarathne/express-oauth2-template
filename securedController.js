ee7d655da5dbe6705ec9139082b16e836e9a32c0
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 09:40:45 +0530

    Update securedController.js

--- securedController.js
+++ securedController.js
@@ -1,6 +1,7 @@
 const authMiddleware = require('./authMiddleware');
 
-
+const express = require('express');
+const router = express.Router();
 
 router.get('/secured', authMiddleware, (req, res) => {
   res.send('This is a secured route');
