b0458ceb994855798c867e2f3a4c39ba34751758
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 07:59:11 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// TODO load from env
 mongoose.connect('mongodb://localhost/jwt_auth', { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = mongoose;
