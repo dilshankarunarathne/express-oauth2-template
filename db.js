8ff7595897875d74f3209f95279bbbd2ec43af63
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 09:26:01 +0530

    dotenv configured

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// TODO load from env
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = mongoose;
