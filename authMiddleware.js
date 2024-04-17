497d76439576aa4163d5477159b39bb49c275ba6
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 08:00:22 +0530

    Update authMiddleware.js

--- authMiddleware.js
+++ authMiddleware.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   if (!token) return res.sendStatus(401);
-  jwt.verify(token, 'secret_key', (err, user) => {
+  jwt.verify(token, 'xxxxxxxxxxxxxxxxxxxxxx', (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
     next();
