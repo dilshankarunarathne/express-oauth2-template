71dcfc6d91f4ae59f5aca972d19476a236685d53
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 09:32:05 +0530

    added CORS

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const db = require('./db');
 const authController = require('./authController');
 
+app.use(cors()); 
 app.use(express.json());
 app.use('/auth', authController);
 
