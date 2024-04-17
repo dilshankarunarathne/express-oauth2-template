783b14e3f9a664b4d898f98fc800c8537bd195f9
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 08:00:48 +0530

    Update authController.js

---
+++ authController.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('./User');
+
+router.post('/signup', async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const user = new User({ username: req.body.username, password: hashedPassword });
+  await user.save();
+  res.sendStatus(201);
+});
+
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+  if (!user || !await bcrypt.compare(req.body.password, user.password)) {
+    return res.sendStatus(401);
+  }
+  const token = jwt.sign({ _id: user._id }, 'secret_key');
+  res.send({ token });
+});
+
+module.exports = router;
